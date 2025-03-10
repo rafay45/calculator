f7e132110ad8ca6284acc573dbe22fdf9ff1e042
rafay45 <rafayy579@gmail.com>
2025-03-10 13:03:54 +0500

    more addition

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let display = document.getElementById('output');
 
+
+function onOff() {
+    if (display.value === "") {
+        display.value = "0";
+    } else if (display.value === display.value) {
+        display.value = ""
+    }
+}
 function appendValue(event) {
     display.value += event
 }
@@ -14,12 +22,5 @@ function equal(){
         display.value = eval(display.value);
     } catch (error) {
         alert("Invalid Calculation!");
-    }
-}
-function onOff() {
-    if (display.value === "") {
-        display.value = "0";
-    } else if (display.value === display.value) {
-        display.value = ""
     }
 }
