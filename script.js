a268baba5498fca47c217b0552b7f8388cd67da3
rafay45 <rafayy579@gmail.com>
2025-03-11 14:34:44 +0500

    more addition

--- script.js
+++ script.js
@@ -1,26 +1,48 @@
 let display = document.getElementById('output');
-
-
+display.value = "off"
 function onOff() {
-    if (display.value === "") {
-        display.value = "0";
-    } else if (display.value === display.value) {
-        display.value = ""
+    if(display.value === "off"){
+        display.value = "on";
+    }else{
+        display.value = "off"
     }
 }
 function appendValue(event) {
-    display.value += event
-}
-function clearDisplay() {
-    display.value = "0"
-}
-function deleteDisplay() {
-    display.value = display.value.slice(0, -1)
-}
-function equal(){
-    try {
-        display.value = eval(display.value);
-    } catch (error) {
-        alert("Invalid Calculation!");
+    if (display.value === "off") {
+        display.value = "off"
+    } else if (display.value === "on") {
+        display.value = "";
+    } else if (display.value === "0") {
+        display.value = ""
+    } else {
+        display.value += event
     }
+}
+
+function clearDisplay() {
+    if (display.value === "off") {
+        display.value = "off"
+    } else {
+        display.value = "0";
+    }
+}
+
+function deleteDisplay() {
+    if (display.value === "off") {
+        display.value = "off"
+    } else {
+        display.value = display.value.slice(0, -1)
+    }
+}
+function equal() {
+    if (display.value === "off") {
+        display.value = "off"
+    } else {
+        try {
+            display.value = eval(display.value);
+        } catch (error) {
+            alert("Invalid Calculation!");
+        }
+    }
+
 }
