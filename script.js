057b8486e860fe1d3863a61d2cbbd779a299635e
rafay45 <rafayy579@gmail.com>
2025-05-07 22:03:58 +0500

    🧮🤞🏻Completed

--- script.js
+++ script.js
@@ -1,42 +1,51 @@
 let display = document.getElementById('output');
-function onOff() {
-    if (display.style.backgroundColor = "rgb(32, 32, 32)") {
-        display.style.backgroundColor = "rgb(48, 47, 47)";
+function on() {
+    if (display.style.backgroundColor === "rgb(10, 10, 10)") {
+        display.style.backgroundColor = "rgb(48, 47, 47)"
+    }else{
+        display.style.backgroundColor = "rgb(48, 47, 47)"
     }
-    else if (display.style.backgroundColor = "rgb(48, 47, 47)") {
-        display.style.backgroundColor = "rgb(32, 32, 32)";
+}
+function off() {
+    if (display.style.backgroundColor === "rgb(48, 47, 47)") {
+        display.style.backgroundColor = "rgb(10, 10, 10)"
+        display.value = ""
+    } else{
+        display.style.backgroundColor = "rgb(10, 10, 10)"
     }
 }
 function appendValue(event) {
-    if (display.style.backgroundColor = "rgb(32, 32, 32)") {
-        display.style.backgroundColor = "rgb(48, 47, 47)";
+    if (display.style.backgroundColor === "rgb(48, 47, 47)") {
+        display.value += event
     } else if (display.value === "0") {
         display.value = ""
     } else if (display.value === "syntax error") {
         display.value = ""
     } else {
-        display.value += event
+        display.style.backgroundColor = "rgb(10, 10, 10)"
+        display.value = ""
     }
 }
 
 function clearDisplay() {
-    if (display.value === "off") {
-        display.value = "off"
+    if (display.style.backgroundColor === "rgb(48, 47, 47)") {
+        display.value = ""
     } else {
-        display.value = "0";
+        display.style.backgroundColor = "rgb(10, 10, 10)"
     }
 }
 
 function deleteDisplay() {
-    if (display.value === "off") {
-        display.value = "off"
+    if (display.style.backgroundColor === "rgb(10, 10, 10)") {
+        display.style.backgroundColor = "rgb(10, 10, 10)"
+        display.value = ""
     } else {
         display.value = display.value.slice(0, -1)
     }
 }
 function equal() {
-    if (display.value === "off") {
-        display.value = "off"
+    if (display.style.backgroundColor === "rgb(32, 32, 32)") {
+        display.style.backgroundColor = "rgb(32, 32, 32)"
     } else {
         try {
             display.value = eval(display.value);
